ead869e3bb1ced2bcaf652b70ec2d96ead3894a4
Gabriel Gonzalez <gabokappa@gmail.com>
2019-08-07 15:35:06 +0100

    Question 8 finished

---
+++ directory8.rb
@@ -0,0 +1,96 @@
+#Once you complete the previous exercise, change the way the users are displayed:
+#print them grouped by cohorts. To do this, you'll need to get a list of all
+#existing cohorts (the map() method may be useful but it's not the only option),
+#iterate over it and only print the students from that cohort.
+#MAP returns an array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create  an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+
+cohort_count = 0
+    puts "Which cohort is this person in? Input the month"
+    cohort1 = gets.chomp
+    if cohort1 =~ /^[0-9]/
+      puts "Please input the month name"
+      cohort1 = gets.chomp.downcase.to_sym
+    else
+      cohort1 = cohort1.downcase.to_sym
+    end
+
+    puts "Let me know this student's hobby"
+    hobby = gets.chomp.to_sym
+    if hobby.empty?
+      hobby = ("Calcio").to_sym
+    end
+
+    puts "What is this student's country of birth?"
+    country_of_birth = gets.chomp
+
+    puts "What is the student's height?"
+    height1 = gets.chomp
+    #add the student hash to the array
+    students << {name: name, cohort: cohort1, hobbies: hobby, birth: country_of_birth, height: height1}
+    puts "Now we have #{students.count} students"
+
+    puts "Please enter the name of the next student"
+    name = gets.chomp
+
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+
+  puts "Which cohort would you like to print out?"
+  cohort_group = gets.chomp.downcase.to_sym
+  @count = 0
+  new_list = []
+  students.each do |person|
+    person.each do |_, value|
+      if value == cohort_group
+        new_list << person
+      else
+      end
+    end
+  end
+
+  until @count == new_list.length
+    puts ("#{@count + 1}. #{new_list[@count][:name]} (#{new_list[@count][:cohort]} cohort)").center(60, ' *** ')
+    puts "Hobbies #{new_list[@count][:hobbies]} *** Height: #{new_list[@count][:height]}"
+    puts "Country of birth: #{new_list[@count][:birth]}"
+    puts ""
+    @count += 1
+  end
+end
+
+=begin
+  count = 0
+  until count == students.length
+  students.each_with_index do |student, index|
+    puts ("#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)").center(60, '*** ')
+    puts "Hobbies: #{students[count][:hobbies]} *** Height: #{students[count][:height]}"
+    puts "Country of birth: #{students[count][:birth]}"
+    puts ""
+    count += 1
+    end
+  end
+=end
+
+def print_footer(students)
+  puts "Overall, we have #{@count.to_s} great students."
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
