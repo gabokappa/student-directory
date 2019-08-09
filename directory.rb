626408e9f77e9e37b22ecdc9bbfe2a34fd46c1ec
Gabriel Gonzalez <gabokappa@gmail.com>
2019-08-09 09:18:13 +0100

    Step 14.2 - load default file

--- directory.rb
+++ directory.rb
@@ -106,8 +106,10 @@ end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil? #get out of the method if it isn't given
-  if File.exist?(filename) #if it exists
+  if filename.nil?
+    puts "No file name provided, default file students.csv has been loaded."
+    load_students
+  elsif File.exist?(filename) #if it exists
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
